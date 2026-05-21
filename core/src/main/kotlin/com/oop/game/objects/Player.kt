758faab05ffe811c04780b117dba99e0dbe528ee
GEC-알스톰 <gec_thyristor_chopper@[127.0.0.9]>
2026-05-21 15:15:33 +0900

    매개변수명

--- core/src/main/kotlin/com/oop/game/objects/Player.kt
+++ core/src/main/kotlin/com/oop/game/objects/Player.kt
@@ -61,6 +61,7 @@ class Player(
 					holding.shoot(Position(x.toFloat(), y.toFloat()), this@Player);
 					return true;
 				}
+				
 				return false;
 			}
 			
@@ -72,13 +73,13 @@ class Player(
 			
 			override fun touchUp(x: Int, y: Int, pointer: Int, button: Int): Boolean = false;
 			
-			override fun touchCancelled(x: Int, y: Int, p3: Int, p4: Int): Boolean = false;
+			override fun touchCancelled(x: Int, y: Int, pointer: Int, button: Int): Boolean = false;
 			
-			override fun touchDragged(x: Int, y: Int, p3: Int): Boolean = false;
+			override fun touchDragged(x: Int, y: Int, pointer: Int): Boolean = false;
 			
 			override fun mouseMoved(x: Int, y: Int): Boolean = false;
 			
-			override fun scrolled(p1: Float, p2: Float): Boolean = false;
+			override fun scrolled(amountX: Float, amountY: Float): Boolean = false;
 		});
 	}
 
