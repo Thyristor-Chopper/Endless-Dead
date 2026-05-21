f055feb13361ddfc882fac71ff62214af451c6a5
kimharang <KIMharang012@gmail.com>
2026-05-21 13:55:12 +0900

    주석과 머신건 뼈대

--- core/src/main/kotlin/com/oop/game/Gun.kt
+++ core/src/main/kotlin/com/oop/game/Gun.kt
@@ -9,13 +9,22 @@ abstract class Gun(world: GameWorld, id: String, name: String, damage: Int, spee
 		val bullet = Bullet(world, shooter.x, shooter.y, target, bulletSpeed, bulletDamage);
 		world.add(bullet);
 	}
-}
+}//maxAmmo와 ammo 프로퍼티도 필요할 듯? fireinterval, fire 함수도
 
 class shotGun(world: GameWorld): Gun(
 	world,
 	"G002",
 	"샷건",
 	20,
+	5f
+){
+// 기능들 넣을까 예정
+}
+class machineGun(world: GameWorld) : Gun(
+	world,
+	"G003",
+	"기관총",
+	5,
 	5f
 ){
 
