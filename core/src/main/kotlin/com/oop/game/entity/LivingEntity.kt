5f78212705847dbbf982bc965094ad39ef2871b5
GEC-알스톰 <gec_thyristor_chopper@[127.0.0.1]>
2026-05-22 23:39:35 +0900

    invincibilityTimer 음수 방지

--- core/src/main/kotlin/com/oop/game/entity/LivingEntity.kt
+++ core/src/main/kotlin/com/oop/game/entity/LivingEntity.kt
@@ -16,11 +16,14 @@ abstract class LivingEntity(world: World, x: Float, y: Float, width: Float, heig
 
 	// 피격 시 잠깐 동안 데미지를 안 받게 해주는 무적 타이머. 자식들도 알 수 있게 protected로 설정.
 	protected var invincibilityTimer: Float = 0f
-		private set
+		private set(value) {
+			if(value < 0) field = 0;
+			else field = value;
+		}
 
 	open fun takeDamage(damage: Int, duration: Float = 0f) {
 		// 무적 시간이 다 끝났을 때만 피격당함
-		if (invincibilityTimer <= 0f) {
+		if (invincibilityTimer == 0f) {
 			if(damage > 0) hp -= damage
 			invincibilityTimer = duration  // 한 대 맞았으니 지정된 시간만큼 무적 켤게!
 		}
