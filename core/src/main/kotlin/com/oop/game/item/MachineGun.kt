b4abbe77dc304140d0ff3a68405c7c588a250d45
GEC-알스톰 <gec_thyristor_chopper@[127.0.0.1]>
2026-05-24 00:09:33 +0900

    음

--- core/src/main/kotlin/com/oop/game/item/MachineGun.kt
+++ core/src/main/kotlin/com/oop/game/item/MachineGun.kt
@@ -2,6 +2,6 @@ package com.oop.game.item;
 
 import com.oop.game.world.World;
 
-class MachineGun(world: World) : Gun(world, "G003", "Machine Gun", 5, 500f, false, 0.1f, 30, 30) {
+class MachineGun(world: World) : Gun(world, "G003", "Machine Gun", 5, 500f, false, 0.15f, 30, 30) {
 	
 }
