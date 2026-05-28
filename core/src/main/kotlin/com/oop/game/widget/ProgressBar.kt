10200038a5cb319017632643a3bbc9fa9fde24c6
GEC-알스톰 <gec_thyristor_chopper@[127.0.0.1]>
2026-05-29 01:37:50 +0900

    청크된 미터기 구현

--- core/src/main/kotlin/com/oop/game/widget/ProgressBar.kt
+++ core/src/main/kotlin/com/oop/game/widget/ProgressBar.kt
@@ -6,12 +6,23 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.NinePatch;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
+import kotlin.math.ceil;
+
 private const val BAR_VERTICAL_PADDING = 3;
 private const val BAR_HORIZONTAL_PADDING = 3;
+private const val CHUNK_WIDTH = 6;
+private const val CHUNK_MARGIN = 2;
 
 class ProgressBar(x: Float, y: Float, width: Float, height: Float = 15f, var value: Float = 0f, val color: Color = Color.WHITE) : Widget(x, y, width, height) {
+	enum class Style {
+		CHUNKED,
+		SMOOTH;
+	}
+	
+	var style = Style.CHUNKED;
 	private val rawBarTexture = Texture(Gdx.files.internal("progress_bar.bmp"));
-	private val rawIndicatorTexture = Texture(Gdx.files.internal("progress_chunk.bmp"));
+	private val rawIndicatorTexture = Texture(Gdx.files.internal("progress_indicator.bmp"));
+	private val chunkTexture = Texture(Gdx.files.internal("progress_chunk.bmp"));
 	private val barTexture = NinePatch(rawBarTexture, 2, 2, 5, 6);
 	private val indicatorTexture = NinePatch(rawIndicatorTexture, 1, 1, 2, 1);
 	
@@ -20,13 +31,32 @@ class ProgressBar(x: Float, y: Float, width: Float, height: Float = 15f, var val
 		barTexture.draw(batch, x, y, width, height);
 		if(value > 0f) {
 			batch.color = color;
-			indicatorTexture.draw(batch, x + BAR_HORIZONTAL_PADDING, y + BAR_VERTICAL_PADDING, (width - BAR_HORIZONTAL_PADDING * 2) * value, height - BAR_VERTICAL_PADDING * 2);
+			val drawableBarWidth = (width - BAR_HORIZONTAL_PADDING * 2);
+			val indicatorWidth = drawableBarWidth * value;
+			when(style) {
+				Style.CHUNKED	-> {
+					val chunkCount = ceil(indicatorWidth / (CHUNK_WIDTH + CHUNK_MARGIN)).toInt();
+					for(i in 1..chunkCount) {
+						val x = this.x + BAR_HORIZONTAL_PADDING + (CHUNK_WIDTH + CHUNK_MARGIN) * (i - 1);
+						val width = 
+							if(i == chunkCount && x - this.x + CHUNK_WIDTH - CHUNK_MARGIN - 1 > drawableBarWidth)
+								CHUNK_WIDTH - (x - this.x + CHUNK_WIDTH - CHUNK_MARGIN - 1 - drawableBarWidth)
+							else
+								CHUNK_WIDTH.toFloat();
+						batch.draw(chunkTexture, x, y + BAR_VERTICAL_PADDING, width, height - BAR_VERTICAL_PADDING * 2);
+					}
+				}
+				Style.SMOOTH	-> {
+					indicatorTexture.draw(batch, x + BAR_HORIZONTAL_PADDING, y + BAR_VERTICAL_PADDING, indicatorWidth, height - BAR_VERTICAL_PADDING * 2);
+				}
+			}
 			batch.color = Color.WHITE;
 		}
 	}
 	
 	override fun dispose() {
 		rawBarTexture.dispose();
+		chunkTexture.dispose();
 		rawIndicatorTexture.dispose();
 	}
 }
