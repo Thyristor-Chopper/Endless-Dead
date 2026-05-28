3ac3fda23173ede580d76b3d3446615e5be632e8
Park Chan-hee <chanhee2018@naver.com>
2026-05-28 13:37:28 +0900

    shoes 아이템으로 속도 올리는 함수는 구현했고 실제 속도 올리는건 player쪽에 넣겠습니다

---
+++ core/src/main/kotlin/com/oop/game/item/shoes.kt
@@ -0,0 +1,22 @@
+package com.oop.game.item;
+
+import com.oop.game.world.World;
+
+/**
+ * 신발 - 속도 아이템
+ *
+ * @param world	아이템이 있는 세계
+ */
+class Shoes(world: World) : Item(world, "shoes", "Shoes"), Usable {
+	override val allowContinuousUse = false;
+	
+	/**
+	 * 신발을 사용하여 속도를 1만큼 올린다
+	 */
+	override fun use(): Boolean {
+		world.player.speed_up(50f);
+		world.drawSubtitles("SPEED UP");
+		destroy();
+		return true;
+	}
+}
