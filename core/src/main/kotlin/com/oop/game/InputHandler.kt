ebd3909b7b8074a78e1c41dffb7b4dfc8c05314f
GEC-알스톰 <gec_thyristor_chopper@[127.0.0.1]>
2026-05-28 22:28:14 +0900

    아이템 파괴

--- core/src/main/kotlin/com/oop/game/InputHandler.kt
+++ core/src/main/kotlin/com/oop/game/InputHandler.kt
@@ -74,7 +74,7 @@ object InputHandler {
     const val A = Input.Keys.A;
     const val S = Input.Keys.S;
     const val D = Input.Keys.D;
-    const val DELETE = Input.Keys.DEL;
+    const val DELETE = Input.Keys.FORWARD_DEL;
     const val LEFT_MOUSE = Input.Buttons.LEFT;
     const val RIGHT_MOUSE = Input.Buttons.RIGHT;
 }
