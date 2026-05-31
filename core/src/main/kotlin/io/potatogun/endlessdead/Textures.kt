3fbd4f554a63a0d43f1927db8929b80107be8a48
GEC-알스톰 <gec_thyristor_chopper@[127.0.0.1]>
2026-06-01 00:18:45 +0900

    lazy 위임

--- core/src/main/kotlin/io/potatogun/endlessdead/Textures.kt
+++ core/src/main/kotlin/io/potatogun/endlessdead/Textures.kt
@@ -13,26 +13,26 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
  * 여기에서 미리 정의된 텍스처는 Entity#dispose가 아닌 Game#dispose에서 정리된다.
  */
 object Textures {
-	private val shared = mapOf<String, Texture>(
-		"building" to loadTexture("building.bmp"),
-		"building_player_item" to loadTexture("building_player_added.bmp"),
-		"bullet" to loadTexture("bullet.bmp"),
-		"chest" to loadTexture("chest.bmp"),
-		"chest_player_item" to loadTexture("chest_player_added.bmp"),
-		"empty_building" to loadTexture("building_empty.bmp"),
-		"empty_chest" to loadTexture("chest_empty.bmp"),
-		"progress_bar" to loadTexture("progress_bar.bmp"),
-		"progress_fill" to loadTexture("progress_chunk.bmp"),
-		"zombie" to loadTexture("zombie.bmp"),
+	private val shared = mapOf<String, Lazy<Texture>>(
+		"building" to lazy { loadTexture("building.bmp") },
+		"building_player_item" to lazy { loadTexture("building_player_added.bmp") },
+		"bullet" to lazy { loadTexture("bullet.bmp") },
+		"chest" to lazy { loadTexture("chest.bmp") },
+		"chest_player_item" to lazy { loadTexture("chest_player_added.bmp") },
+		"empty_building" to lazy { loadTexture("building_empty.bmp") },
+		"empty_chest" to lazy { loadTexture("chest_empty.bmp") },
+		"progress_bar" to lazy { loadTexture("progress_bar.bmp") },
+		"progress_fill" to lazy { loadTexture("progress_chunk.bmp") },
+		"zombie" to lazy { loadTexture("zombie.bmp") },
 	);
-	val progressBar: NinePatch by lazy { NinePatch(shared["progress_bar"]!!, 2, 2, 5, 6) };
-	val progressChunkedFill: NinePatch by lazy { NinePatch(TextureRegion(shared["progress_fill"]!!, 1, 0, 1, shared["progress_fill"]!!.getHeight()), 0, 0, 1, 1) };
-	val progressSmoothFill: NinePatch by lazy { NinePatch(shared["progress_fill"]!!, 1, 1, 1, 1) };
+	val progressBar: NinePatch by lazy { NinePatch(shared["progress_bar"]!!.value, 2, 2, 5, 6) };
+	val progressChunkedFill: NinePatch by lazy { NinePatch(TextureRegion(shared["progress_fill"]!!.value, 1, 0, 1, shared["progress_fill"]!!.value.getHeight()), 0, 0, 1, 1) };
+	val progressSmoothFill: NinePatch by lazy { NinePatch(shared["progress_fill"]!!.value, 1, 1, 1, 1) };
 
 	/**
 	 * 지정한 이름의 공유 텍스처를 가져온다.
 	 */
-	fun getShared(id: String): Texture = shared[id] ?: throw IllegalArgumentException("invalid shared texture ID");
+	fun getShared(id: String): Texture = shared[id]?.value ?: throw IllegalArgumentException("invalid shared texture ID");
 
 	/**
 	 * 지정한 화일 이름의 텍스처를 가져온다.
@@ -48,6 +48,7 @@ object Textures {
 	 */
 	fun disposeShared() {
 		for(texture in shared.values)
-			texture.dispose();
+			if(texture.isInitialized())
+				texture.value.dispose();
 	}
 }
