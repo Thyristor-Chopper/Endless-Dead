f01270ed3f47db613c9e7cffb63660a7d91b4993
GEC-알스톰 <gec_thyristor_chopper@[127.0.0.1]>
2026-05-31 13:43:11 +0900

    미사용 필드

--- core/src/main/kotlin/io/potatogun/endlessdead/Timer.kt
+++ core/src/main/kotlin/io/potatogun/endlessdead/Timer.kt
@@ -12,12 +12,6 @@ import com.badlogic.gdx.utils.Timer.Task;
  * @param operation		실행할 서브루틴
  */
 class Timer(val interval: Float, val delay: Float = interval, val onlyInPlay: Boolean = true, private val operation: () -> Unit) {
-	private var delta = interval - 1
-		set(value) {
-			if(value < 0f) field = 0f;
-			else if(value >= interval) field = interval - 1f;
-			else field = value;
-		};
 	private val task = object : Task() {
 		override fun run() {
 			if(!onlyInPlay || (onlyInPlay && GameManager.isPlaying))
