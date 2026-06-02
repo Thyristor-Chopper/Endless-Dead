3ab63819c60814b1af3e0d8da90cba753618dd30
GEC-알스톰 <gec_thyristor_chopper@[127.0.0.1]>
2026-06-03 00:40:12 +0900

    주석

--- core/src/main/kotlin/io/potatogun/endlessdead/Window.kt
+++ core/src/main/kotlin/io/potatogun/endlessdead/Window.kt
@@ -40,6 +40,11 @@ object Window {
 
 	/**
 	 * 창 크기 캐시를 최신화한다.
+	 *
+	 * Game#resize에서 width, height 인자를 받아오면 Gdx.graphics.getWidth() 콜 오버헤드마저 없어져서
+	 *   더 극단적인 최적화가 가능하지만 이렇게 하면 다른 클래스에서 이상한 너비 높이 값으로
+	 *   updateWindowDimensions을 호출할 위험이 있다. 그래도 최적화와 안전의 균형은
+	 *   지키는 게 나을 수도.
 	 */
 	internal fun updateWindowDimensions() {
 		val floatWidth = Gdx.graphics.width.toFloat();
