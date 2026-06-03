8e672166f8901ad1e602ec8dcc5d1deb1b13d065
GEC-알스톰 <gec_thyristor_chopper@[127.0.0.1]>
2026-06-04 02:32:54 +0900

    덮어씌워진 주석 복구

--- core/src/main/kotlin/io/potatogun/endlessdead/item/MachineGun.kt
+++ core/src/main/kotlin/io/potatogun/endlessdead/item/MachineGun.kt
@@ -4,6 +4,8 @@ import io.potatogun.gdxhelper.world.World;
 
 /**
  * 기관총
+ *
+ * 기관총은 allowContinuousUse을 true로 놓아 키다운으로도 연사 가능
  */
 class MachineGun(world: World) : Gun(world, "machine_gun", "Machine Gun", 5, 500f, 2, true, 0.1f, 30, 30) {
 	override val isContinuousUseAllowed = true;
