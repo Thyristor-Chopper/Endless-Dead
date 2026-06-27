62257887f8c385c6fbf66fe5f9cfb9f15407fcc5
GEC-알스톰 <gec_thyristor_chopper@[127.0.0.1]>
2026-06-27 16:59:02 +0900

    ㅇㅁㅁ

--- packcode.bat
+++ packcode.bat
@@ -1,16 +1,16 @@
 for /r %%i in (*.kt) do (
     echo ---- file: %%i ---->>src_packed.txt
-    echo.>>src_packed.txt
+    echo(>>src_packed.txt
     type "%%i">>src_packed.txt
-    echo.>>src_packed.txt
-    echo.>>src_packed.txt
-    echo.>>src_packed.txt
+    echo(>>src_packed.txt
+    echo(>>src_packed.txt
+    echo(>>src_packed.txt
 )
 for /r %%i in (*.java) do (
     echo ---- file: %%i ---->>src_packed.txt
-    echo.>>src_packed.txt
+    echo(>>src_packed.txt
     type "%%i">>src_packed.txt
-    echo.>>src_packed.txt
-    echo.>>src_packed.txt
-    echo.>>src_packed.txt
+    echo(>>src_packed.txt
+    echo(>>src_packed.txt
+    echo(>>src_packed.txt
 )
