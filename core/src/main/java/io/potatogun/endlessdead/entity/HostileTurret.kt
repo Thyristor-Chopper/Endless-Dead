10a72ccc57e37e1f1e344a9bd8d66514258af1c1
GEC-알스톰 <gec_thyristor_chopper@[127.0.0.1]>
2026-06-28 00:03:26 +0900

    약간의 추가

--- core/src/main/java/io/potatogun/endlessdead/entity/HostileTurret.kt
+++ core/src/main/java/io/potatogun/endlessdead/entity/HostileTurret.kt
@@ -4,6 +4,7 @@ import io.potatogun.endlessdead.Textures;
 import io.potatogun.endlessdead.item.Gun;
 import io.potatogun.endlessdead.item.Rarity;
 import io.potatogun.endlessdead.world.SinglePlayerWorld;
+import io.potatogun.gdxhelper.entity.Entity;
 import io.potatogun.gdxhelper.util.getDistanceSorted;
 import io.potatogun.gdxhelper.world.World;
 
@@ -15,10 +16,16 @@ import io.potatogun.gdxhelper.world.World;
  * @param y           Y 좌표
  * @param isPermanent 포탑이 영구적인지의 여부(죽지 못하는지)
  */
-class HostileTurret(world: World, x: Float, y: Float, isPermanent: Boolean = false) : Turret(world, "Turret", x, y, HostileTurretGun(), 600, isPermanent, Textures.getShared("turret_hostile")) {
+class HostileTurret(world: World, x: Float, y: Float, isPermanent: Boolean = false) : Turret(world, "Turret", x, y, HostileTurretGun(), 600, isPermanent, Textures.getShared("turret_hostile")), DamageListener {
 	init {
 		setTargetFetcher { if(world is SinglePlayerWorld) world.player else world.entities.getDistanceSorted(this).firstOrNull { it is Player } as? Player };
 		setFollowRange(384f);
+	}
+
+	// 누군가가 포탑을 공격하면 처치 대상을 그자로 한다.
+	override fun onDamage(damage: Int, attacker: Entity?) {
+		if(attacker is LivingEntity)
+			target = attacker;
 	}
 
 	/**
