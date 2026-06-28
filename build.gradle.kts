fcf1989da08a5cb46c2f82d87c749d16f19da6af
GEC-알스톰 <gec_thyristor_chopper@[127.0.0.1]>
2026-06-28 14:07:24 +0900

    람다 최적화

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,6 +18,7 @@ subprojects {
 	tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
 		kotlinOptions {
 			jvmTarget = "1.8"  // Windows XP에서 실행하기 위함
+			freeCompilerArgs += listOf("-Xlambdas=indy")
 		}
 	}
 }
