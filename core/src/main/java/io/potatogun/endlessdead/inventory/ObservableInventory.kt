f4a3a03fc35a065b6f4b635f48c60db302251aea
GEC-알스톰 <gec_thyristor_chopper@[127.0.0.1]>
2026-06-30 19:02:10 +0900

    다듬기, 기능ㄹ 추가, 버그 수정

--- core/src/main/java/io/potatogun/endlessdead/inventory/ObservableInventory.kt
+++ core/src/main/java/io/potatogun/endlessdead/inventory/ObservableInventory.kt
@@ -1,6 +1,7 @@
 package io.potatogun.endlessdead.inventory;
 
 import com.badlogic.gdx.utils.Array as GdxArray;
+import com.badlogic.gdx.utils.ObjectMap;
 
 import io.potatogun.endlessdead.entity.InventoryHolder;
 import io.potatogun.endlessdead.item.Item;
@@ -11,9 +12,12 @@ import java.util.function.Consumer;
  * 이벤트 핸들러가 있는 인벤토리
  */
 abstract class ObservableInventory : Inventory {
-	private val itemAddObservers = GdxArray<(Item) -> Unit>(false, 4);
-	private val itemRemoveObservers = GdxArray<(Item) -> Unit>(false, 4);
-	private val clearObservers = GdxArray<() -> Unit>(false, 4);
+	private val itemAddObservers = GdxArray<(Item) -> Unit>(false, 2);
+	private val javaAddObserverMap = ObjectMap<Consumer<Item>, (Item) -> Unit>(2);
+	private val itemRemoveObservers = GdxArray<(Item) -> Unit>(false, 2);
+	private val javaRemoveObserverMap = ObjectMap<Consumer<Item>, (Item) -> Unit>(2);
+	private val clearObservers = GdxArray<() -> Unit>(false, 2);
+	private val javaClearObserverMap = ObjectMap<Runnable, () -> Unit>(2);
 
 	protected fun invokeItemAddObservers(item: Item) {
 		for(i in 0 until itemAddObservers.size)
@@ -45,7 +49,30 @@ abstract class ObservableInventory : Inventory {
 	 * @param handler 콜백, Item: 추가된 아이템
 	 */
 	fun addItemAddObserver(handler: Consumer<Item>) {
-		itemAddObservers.add { handler.accept(it) };
+		val ktHandler: (Item) -> Unit = { handler.accept(it) };
+		javaAddObserverMap.put(handler, ktHandler);
+		itemAddObservers.add(ktHandler);
+	}
+
+	/**
+	 * 아이템이 추가될 때 호출되는 콜백 함수를 해제한다.
+	 *
+	 * @param handler 해제할 콜백
+	 */
+	fun removeItemAddObserver(handler: (Item) -> Unit) {
+		itemAddObservers.removeValue(handler, true);
+	}
+
+	/**
+	 * 아이템이 추가될 때 호출되는 콜백 함수를 해제한다 (자바에서 사용).
+	 *
+	 * @param handler 해제할 콜백
+	 */
+	fun removeItemAddObserver(handler: Consumer<Item>) {
+		javaAddObserverMap[handler]?.let {
+			itemAddObservers.removeValue(it, true);
+			javaAddObserverMap.remove(handler);
+		};
 	}
 
 	/**
@@ -63,7 +90,30 @@ abstract class ObservableInventory : Inventory {
 	 * @param handler 콜백, Item: 제거된 아이템
 	 */
 	fun addItemRemoveObserver(handler: Consumer<Item>) {
-		itemRemoveObservers.add { handler.accept(it) };
+		val ktHandler: (Item) -> Unit = { handler.accept(it) };
+		javaRemoveObserverMap.put(handler, ktHandler);
+		itemRemoveObservers.add(ktHandler);
+	}
+
+	/**
+	 * 아이템이 제거될 때 호출되는 콜백 함수를 해제한다.
+	 *
+	 * @param handler 해제할 콜백
+	 */
+	fun removeItemRemoveObserver(handler: (Item) -> Unit) {
+		itemRemoveObservers.removeValue(handler, true);
+	}
+
+	/**
+	 * 아이템이 제거될 때 호출되는 콜백 함수를 해제한다 (자바에서 사용).
+	 *
+	 * @param handler 해제할 콜백
+	 */
+	fun removeItemRemoveObserver(handler: Consumer<Item>) {
+		javaRemoveObserverMap[handler]?.let {
+			itemRemoveObservers.removeValue(it, true);
+			javaRemoveObserverMap.remove(handler);
+		};
 	}
 
 	/**
@@ -81,6 +131,29 @@ abstract class ObservableInventory : Inventory {
 	 * @param handler 콜백
 	 */
 	fun addClearObserver(handler: Runnable) {
-		clearObservers.add { handler.run() };
+		val ktHandler: () -> Unit = { handler.run() };
+		javaClearObserverMap.put(handler, ktHandler);
+		clearObservers.add(ktHandler);
+	}
+
+	/**
+	 * 인벤토리가 초기화될 때 콜백 함수를 해제한다.
+	 *
+	 * @param handler 해제할 콜백
+	 */
+	fun removeClearObserver(handler: () -> Unit) {
+		clearObservers.removeValue(handler, true);
+	}
+
+	/**
+	 * 인벤토리가 초기화될 때 콜백 함수를 해제한다 (자바에서 사용).
+	 *
+	 * @param handler 해제할 콜백
+	 */
+	fun removeClearObserver(handler: Runnable) {
+		javaClearObserverMap[handler]?.let {
+			clearObservers.removeValue(it, true);
+			javaClearObserverMap.remove(handler);
+		};
 	}
 }
