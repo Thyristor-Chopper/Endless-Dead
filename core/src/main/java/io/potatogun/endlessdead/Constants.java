225edbf7797aaeaa7c149970507764090e46c455
GEC-알스톰 <gec_thyristor_chopper@[127.0.0.1]>
2026-07-04 02:27:14 +0900

    ㅎ

--- core/src/main/java/io/potatogun/endlessdead/Constants.java
+++ core/src/main/java/io/potatogun/endlessdead/Constants.java
@@ -6,6 +6,7 @@ import org.jetbrains.annotations.NotNull;
  * 게임에서 필요한 상수들이다.
  *
  * 불필요한 INSTANCE 필드와 인스턴스를 만드는 오버헤드를 없애기 위해서 그냥 자바로 바꿨음
+ *   이렇게 해도 여전히 상수는 인라인으로 바로 대입된다. (디컴파일해서 확인)
  */
 public final class Constants {
 	private Constants() {
