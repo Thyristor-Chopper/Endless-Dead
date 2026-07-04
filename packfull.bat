a4f864e3ada14ffffda7bbf9940f7fdd0574a7c9
GEC-알스톰 <gec_thyristor_chopper@[127.0.0.1]>
2026-07-04 20:47:38 +0900

    다듬기

---
+++ packfull.bat
@@ -0,0 +1,19 @@
+@echo off
+
+del /q src_packed.txt >nul 2>&1
+
+for /r %%i in (*.kt *.java *.gradle *.kts *.cmd *.sh *.bat *.properties *.gitmodules *.gitignore *.md) do (
+    if /i not "%%~nxi"=="gradlew.bat" (
+        echo Packing: %%i
+        echo ---- file: %%i ---->>src_packed.txt
+        echo(>>src_packed.txt
+        type "%%i">>src_packed.txt
+        echo(>>src_packed.txt
+        echo(>>src_packed.txt
+        echo(>>src_packed.txt
+    )
+)
+
+echo(
+echo Saved to src_packed.txt.
+pause
