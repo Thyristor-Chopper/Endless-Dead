b0743119bdabcb1c4808970821707234062083f9
GEC-알스톰 <gec_thyristor_chopper@[127.0.0.1]>
2026-07-04 23:07:31 +0900

    로그인 수정

--- pushall.bat
+++ pushall.bat
@@ -1,5 +1,7 @@
 @echo off
 cd gdxhelper
+git config credential.helper "!f() { echo username=$GIT_USERNAME; echo password=$GIT_PASSWORD; }; f"
 git push origin master
 cd ..
+git config credential.helper "!f() { echo username=$GIT_USERNAME; echo password=$GIT_PASSWORD; }; f"
 git push origin master
