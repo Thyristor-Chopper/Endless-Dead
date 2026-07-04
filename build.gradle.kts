fc5014e6b6b3ddfe92a7ead197b6c66cbc8b8fdf
GEC-알스톰 <gec_thyristor_chopper@[127.0.0.1]>
2026-07-04 19:35:03 +0900

    음

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,7 +23,7 @@ subprojects {
 			// -Xstring-concat=indy: 문자열 조합 관련 최적화. 자바 8에서는 적용되지 않지만 일단은 넣어둠.
 			// -Xno-call-assertions: 자바 함수 호출 반환 값에 대해 null 검사를 제거한다. 이 정도는 내가 알아서 한다. (-Xno-param-assertions는 안전성을 위해 생략)
 			// -Xno-receiver-assertions: 인라인 함수나 확장함수 호출 시 매개변수 null 검사를 제거한다. 어차피 함수 body 내에서 null 검사를 또 수행하며 인라인 함수는 컴파일러가 먼저 잡는다.
-			freeCompilerArgs.addAll(listOf("-Xlambdas=indy", "-Xstring-concat=indy", "-Xno-call-assertions", "-Xno-receiver-assertions", "-Xno-source-debug-extension", "-Xwarning-level=NOTHING_TO_INLINE:disabled", "-Xwarning-level=UNCHECKED_CAST:disabled"))
+			freeCompilerArgs.addAll(listOf("-Xlambdas=indy", "-Xstring-concat=indy", "-Xno-call-assertions", "-Xno-receiver-assertions", "-Xno-source-debug-extension"))
 
 			// 자바 인터페이스의 default void f() { ... }문법을 쓴다. 디컴파일해서 비교하니까 DefaultImpls 내부 클래스를 만드는 것보다
 			//   훨씬 깔끔하고 효율적이다. (코틀린 1.x은 -Xjvm-default=all)
--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -5,3 +5,9 @@ plugins {
 dependencies {
 	implementation(project(":gdxhelper"))
 }
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+	compilerOptions {
+		freeCompilerArgs.addAll(listOf("-Xwarning-level=NOTHING_TO_INLINE:disabled", "-Xwarning-level=UNCHECKED_CAST:disabled"))
+	}
+}
