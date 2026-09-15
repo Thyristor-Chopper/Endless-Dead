03fe71ec297d7e38a02533457cb46b23cd05e05e
GEC-알스톰 <gec_thyristor_chopper@[127.0.0.9]>
2026-09-16 01:54:42 +0900

    자원 해제 검사 추가

--- core/src/main/java/io/potatogun/endlessdead/EndlessDead.kt
+++ core/src/main/java/io/potatogun/endlessdead/EndlessDead.kt
@@ -7,6 +7,7 @@ import io.potatogun.endlessdead.screen.Title;
 import io.potatogun.endlessdead.screen.ZombieWorldProjector;
 import io.potatogun.gdxhelper.Game;
 import io.potatogun.gdxhelper.Window;
+import io.potatogun.gdxhelper.screen.WorldProjector;
 
 /**
  * 좀비 파밍 게임의 메인 본체
@@ -14,13 +15,11 @@ import io.potatogun.gdxhelper.Window;
 class EndlessDead : Game() {
 	// 미리 등록된 스크린. val은 lateinit이 불가하여 lazy 위임 사용.
 	internal val titleScreen: Title by lazy { Title(this) };
-	internal val worldViewer: ZombieWorldProjector by lazy { ZombieWorldProjector(this) };
+	internal val worldViewer: WorldProjector by lazy { ZombieWorldProjector(this) };
 
-	/**
-	 * 게임 시작 시 한 번 호출되는 메쏘드
-	 */
+	// 게임 시작 시 한 번 호출되는 메쏘드
 	override fun create() {
-		setScreen(titleScreen);  // 부모 Game이 제공하는 메서드
+		setScreen(titleScreen);
 		Window.setBaseTitle(Constants.GAME_TITLE);
 	}
 
@@ -29,9 +28,7 @@ class EndlessDead : Game() {
 		GameManager.tickGameTime(delta);
 	}
 
-	/**
-	 * 자원을 정리한다.
-	 */
+	// 자원을 정리한다.
 	override fun dispose() {
 		super.dispose();
 		Textures.disposeShared();  // 공유 자원 정리
