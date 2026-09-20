ed88e1ec332b29d77c461d80c25af367b14c1b6d
싸이리스터 초퍼제어 <gec_thyristor_chopper@[127.0.0.9]>
2026-09-20 21:12:36 +0900

    매킨토시 관련 플래그를 윈도우 머신에서 빌드해도 작동하게

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,7 +35,7 @@ subprojects {
 			// 최적화
 			//   -Xlambdas=indy: 코틀린 람다 함수를 invoke 메쏘드가 있는 별도 클래스를 만드는 대신 'invoke dynamic'를 사용한다. 디컴파일해서 비교했을 때 '메쏘드명$lambda$순번' static method을 만든다.
 			//   -Xstring-concat=indy: 문자열 조합 관련 최적화. 자바 8에서는 적용되지 않지만 일단은 넣어둠.
-			//   -Xno-call-assertions: 자바 함수 호출 반환 값에 대해 null 검사를 제거한다. 이 정도는 내가 알아서 한다. (-Xno-param-assertions는 안전성을 위해 생략)
+			//   -Xno-call-assertions: 자바 함수 호출 반환 값에 대해 null 검사를 제거한다. 이 정도는 내가 알아서 한다. (-Xno-param-assertions는 desktop을 제외하고는 안전을 위해 생략)
 			//   -Xno-receiver-assertions: 인라인 함수나 확장함수 호출 시 매개변수 null 검사를 제거한다. 어차피 함수 body 내에서 null 검사를 또 수행하며 인라인 함수는 컴파일러가 먼저 잡는다.
 			freeCompilerArgs.addAll(listOf("-Xlambdas=indy", "-Xstring-concat=indy", "-Xno-call-assertions", "-Xno-receiver-assertions", "-Xno-source-debug-extension"))
 			freeCompilerArgs.addAll(listOf("-Xwarning-level=NOTHING_TO_INLINE:disabled", "-Xwarning-level=UNCHECKED_CAST:disabled"))
