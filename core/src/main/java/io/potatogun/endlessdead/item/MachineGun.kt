83f853d87aa614aca1383239926498e14cead152
싸이리스터 초퍼제어 <gec_thyristor_chopper@[127.0.0.1]>
2026-09-22 21:18:23 +0900

    기관총 상향

--- core/src/main/java/io/potatogun/endlessdead/item/MachineGun.kt
+++ core/src/main/java/io/potatogun/endlessdead/item/MachineGun.kt
@@ -5,6 +5,6 @@ package io.potatogun.endlessdead.item;
  *
  * allowContinuousUse을 true로 놓아 키다운으로도 연사 가능하다.
  */
-class MachineGun : Gun("machine_gun", "Machine Gun", Gun.Properties(5, 500f).bulletPenetration(2).fireInterval(0.1f).bullets(40)) {
+class MachineGun : Gun("machine_gun", "Machine Gun", Gun.Properties(5, 500f).bulletPenetration(2).fireInterval(0.1f).bullets(70)) {
 	override val isContinuousUseAllowed = true;
 }
