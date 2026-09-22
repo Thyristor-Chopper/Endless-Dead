d731804aeb02591488ae6cefd230fb58b124d0d4
싸이리스터 초퍼제어 <gec_thyristor_chopper@[127.0.0.9]>
2026-09-22 09:36:25 +0900

    완전히 같은 것

--- core/src/main/java/io/potatogun/endlessdead/entity/ZombieSummoner.kt
+++ core/src/main/java/io/potatogun/endlessdead/entity/ZombieSummoner.kt
@@ -45,7 +45,7 @@ class ZombieSummoner(world: World, x: Float, y: Float) : LivingEntity(world, "Zo
 		if(isActive && player != null)
 			rotateTo(player);
 		else
-			rotateBy(30f * delta);  // 30 ≒ 0.5 / (1 / 60) - 60fps 기준으로 0.5도씩 회전하되 프레임률에 영향을 받지 않게 한다.
+			rotateBy(30f * delta);  // 30 == 0.5 / (1 / 60) - 60fps 기준으로 0.5도씩 회전하되 프레임률에 영향을 받지 않게 한다.
 	}
 
 	private fun spawn() {
