692d463bcffe72c30413377a514fccc96d03c60c
싸이리스터 초퍼제어 <gec_thyristor_chopper@[127.0.0.1]>
2026-09-25 17:43:14 +0900

    스포너는 추상 클래스가 적절할 것 같다

--- core/src/main/java/io/potatogun/endlessdead/spawner/Spawner.kt
+++ core/src/main/java/io/potatogun/endlessdead/spawner/Spawner.kt
@@ -5,11 +5,11 @@ import io.potatogun.gdxhelper.world.World;
 /**
  * 개체 소환기
  */
-interface Spawner {
+abstract class Spawner {
 	/**
 	 * 매 프레임 실행하는 서브루틴
 	 *
 	 * @param delta 직전 프레임과의 간격(초)
 	 */
-	fun update(delta: Float);
+	abstract fun update(delta: Float);
 }
